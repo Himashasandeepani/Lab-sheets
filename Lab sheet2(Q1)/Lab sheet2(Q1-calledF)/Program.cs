c136338c773a6d366c3d7223d0d8575d78b03abc
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2024-02-07 19:10:51 +0530

    Lab sheet 2

---
+++ Lab sheet2(Q1)/Lab sheet2(Q1-calledF)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet2_Q1_calledF_
+{
+    internal class Program
+    {
+       // we can use separate function (program) to calle function.
+/*
+        static void Main()
+        {
+            Book book = new Book();
+            book.Author = "Martin Wikramesinghe";
+            book.Title = "Madol Duwa";
+
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+        }
+*/
+    }
+}
---
+++ Lab sheet2(Q2)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet2_Q2_
+{
+    public class BankAccount
+    {
+        public int AccountNumber;
+        public Double Balance;
+
+        //create a constructor.
+        /*
+        public BankAccount(double InitialBalance)
+        {
+            Balance = InitialBalance;
+        }
+        */
+        public void deposite(double amount)
+        {
+            //updating balance
+            Balance += amount;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if use constructor  initialized values.
+            // BankAccount account = new BankAccount(1000);
+            BankAccount account = new BankAccount();
+
+
+            Console.Write("Enter the account number : ");
+            account.AccountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the deposite amount : ");
+            double amount = double.Parse(Console.ReadLine());
+
+            account.deposite(amount);
+
+            Console.WriteLine("Account number : " +account.AccountNumber);
+            Console.WriteLine("Update balance : " + account.Balance);
+
+            Console.ReadLine();
+
+        }
+    }
+}
