d67ba9e3c7a844b337e5620be06447e10efe284f
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2024-03-14 22:20:28 +0530

    Lab sheet3

---
+++ Lab  sheet3(Q2)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__sheet3_Q2_
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Lab_sheet_03_Q2
+    {
+
+        public class Employee
+        {
+            protected int employeeID;
+            private string FullName;
+            private double salary;
+
+            public int getEmployeeID()
+            {
+                return this.employeeID;
+            }
+
+            //constructor
+            public Employee(int employeeId, String FullName, double salary)
+            {
+                this.employeeID = employeeId;
+                this.FullName = FullName;
+                this.salary = salary;
+            }
+
+            public void DisplayEmployeeInfo()
+            {
+                Console.WriteLine("Employee ID: " + employeeID);
+                Console.WriteLine("Employee Name: " + FullName);
+                Console.WriteLine("Salary: " + salary);
+            }
+        }
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee em = new Employee(101, "saman", 50000);
+                em.DisplayEmployeeInfo();
+                Console.WriteLine("Reading the employee id: " + em.getEmployeeID());
+
+
+                em.salary = 60000;
+            }
+        }
+    }
+
+}
